bb37211120202e3b822883f95c33bf35902b042f
Quin <quinsykora@gmail.com>
2018-06-15 12:43:17 -0400

    Changed art file to only include art and created a sprite movement file

--- UI/movement.py
+++ UI/movement.py
@@ -18,6 +18,8 @@ class MovingObject:
         self.screen = screen
         self.maxy, self.maxx = self.screen.getmaxyx()
         self.load_size(pos)
+        self.pos = pos
+        self.curr_sprite = list(self.sprites.keys())[0]
 
         #pos is meant to be the position of the top left corner of the sprite
 
@@ -27,26 +29,34 @@ class MovingObject:
         for name is self.sprites.keys():
             string = self.sprites[name]
 
-            string = string.split("\n")
+            self.load_single_size(name, string)
 
-            width = 0
-            for line in string:
-                width = max(width, line)
+    def load_single_size(self, name, sprite):
+        string = sprite.split("\n")
 
-            height = len(string)
+        width = 0
+        for line in string:
+            width = max(width, line)
 
-            assert(pos[0] + height < self.maxy)
-            assert(pos[1] + width < self.maxx)
+        height = len(string)
 
-            self.sizes[name] = SizeObject(width=width, height=height)
+        assert(pos[0] + height < self.maxy)
+        assert(pos[1] + width < self.maxx)
+
+        self.sizes[name] = SizeObject(width=width, height=height)
 
     def move(self, start_pos, end_pos):
 
-    def change_sprite(self):
 
-    def add_sprite(self):
+    def change_sprite(self, name):
+
+    def add_sprite(self, name, sprite):
+        self.sprites[name] = sprite
+        self.load_single_size(name, sprite)
 
     def get_curr_pos(self):
+
+    def glide_move(self):
 
 class Object(MovingObject):
     def __init__(self, sprites):
