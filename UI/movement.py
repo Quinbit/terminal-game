0cf2daf7eecbd219097263e7534fa1880b3a021b
Quinlan Sykora <quinlan.sykora@robotics.utias.utoronto.ca>
2018-06-18 08:33:28 -0400

    Moving is now easier

--- UI/movement.py
+++ UI/movement.py
@@ -1,4 +1,5 @@
 import curses
+import math
 
 class SizeObject:
     def __init__(self, width=0, height=0):
@@ -10,16 +11,19 @@ class SizeObject:
         self.HEIGHT = height
 
 class MovingObject:
-    def __init__(self, sprites, screen, pos=(0,0)):
+    def __init__(self, sprites, screen, pos=(0,0), main_display):
 
         #sprites is meant to be a dictionary of names that connect to the various string based sprites
         assert(type(sprites) == dict)
         self.sprites = sprites
         self.screen = screen
+        self.main_display = main_display
         self.maxy, self.maxx = self.screen.getmaxyx()
         self.load_size(pos)
         self.pos = pos
         self.curr_sprite = list(self.sprites.keys())[0]
+        self.next_move = [0,0]
+        self.speed = 0
 
         #pos is meant to be the position of the top left corner of the sprite
 
@@ -45,18 +49,52 @@ class MovingObject:
 
         self.sizes[name] = SizeObject(width=width, height=height)
 
-    def move(self, start_pos, end_pos):
+    def move(self, end_pos):
+        assert(end_pos[0] > 0)
+        assert(end_pos[1] > 0)
+        assert(end_pos[0] + height < self.sizes[self.curr_sprite].HEIGHT)
+        assert(end_pos[1] + width < self.sizes[self.curr_sprite].WIDTH)
 
+        self.pos = end_pos
+        self.main_display.update()
 
     def change_sprite(self, name):
+        assert(name in self.sprites.keys())
+        self.curr_sprite = name
+        self.main_display.update
 
     def add_sprite(self, name, sprite):
         self.sprites[name] = sprite
         self.load_single_size(name, sprite)
 
     def get_curr_pos(self):
+        return self.pos
 
-    def glide_move(self):
+    def glide_move(self, speed, final_change):
+        assert(self.pos[0] + self.next_move[0] + final_size[0] > 0)
+        assert(self.pos[1] + self.next_move[1] + final_size[1] > 0)
+        assert(self.pos[1] + self.next_move[1] + final_size[1] < self.sizes[self.curr_sprite].WIDTH)
+        assert(self.pos[0] + self.next_move[0] + final_size[0] < self.sizes[self.curr_sprite].HEIGHT)
+
+        self.next_move[0] += final_change[0]
+        self.next_move[1] += final_change[1]
+        self.speed = speed
+
+    def update(self):
+        self.pos[0] += self.speed[0]
+        self.pos[1] += self.speed[1]
+
+        self.next_move[0] -= self.speed[0]
+        self.next_move[1] -= self.speed[1]
+
+        if self.next_move[0] < 0:
+            self.pos[0] += self.next_move[0]
+            self.new_move[0] = 0
+
+        if self.next_move[1] < 0:
+            self.pos[1] += self.next_move[1]
+            self.next_move[1] = 0
+
 
 class Object(MovingObject):
     def __init__(self, sprites):
